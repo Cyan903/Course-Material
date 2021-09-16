4ccf6ff9b570af7108b6d7ce6df2d9fcb70f1756
CyanPiano <cyanp0515@gmail.com>
2021-09-16 02:48:50 -0400

    File change vue.js

---
+++ Vue.js/Advanced/first-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<section>
+		<h2>Users</h2>
+		<ul>
+			<li v-for="u in users" :key="u.id">
+				<h2 style="display:inline;">{{ u.name }} </h2>
+				<h3 style="display:inline;">{{ u.age }}</h3>
+			</li>
+		</ul>
+	</section>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				users: [
+					{ id: 0, name: "a", age: 1 },
+					{ id: 1, name: "b", age: 2 },
+					{ id: 2, name: "c", age: 3 },
+					{ id: 3, name: "d", age: 4 },
+					{ id: 4, name: "e", age: 5 },
+					{ id: 5, name: "f", age: 6 },
+				]
+			}
+		}
+	}
+</script>

dcfe6aa5a92fbfe8c27349c6b475124c2768ccde
CyanPiano <cyanp0515@gmail.com>
2021-10-08 12:11:14 -0400

    Vuex start

---
+++ Vue.js/Advanced/10. Vuex/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <base-container title="Vuex">
+    <h3>{{ $store.state.counter }}</h3>
+    <button @click="add">Add 1</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+
+  methods: {
+    add() {
+      this.$store.state.counter++;
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>

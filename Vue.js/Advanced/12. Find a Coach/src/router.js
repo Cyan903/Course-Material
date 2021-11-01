0d729320debcff815a957d46d049b8e6b9fb6557
CyanPiano <cyanp0515@gmail.com>
2021-11-01 01:46:33 -0400

    Optimize for prod (though I'm not pushing it)

--- Vue.js/Advanced/12. Find a Coach/src/router.js
+++ Vue.js/Advanced/12. Find a Coach/src/router.js
@@ -1,12 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-
 import CoachList from "./pages/coaches/CoachList.vue";
-import CoachDetails from "./pages/coaches/CoachDetails.vue";
-import CoachRegister from "./pages/coaches/CoachRegister.vue";
 
-import ContactCoach from "./pages/requests/ContactCoach.vue";
-import RequestRecieved from "./pages/requests/RequestRecieved.vue";
-import UserAuth from "./pages/auth/UserAuth.vue";
+// import CoachDetails from "./pages/coaches/CoachDetails.vue";
+// import CoachRegister from "./pages/coaches/CoachRegister.vue";
+// import ContactCoach from "./pages/requests/ContactCoach.vue";
+// import RequestRecieved from "./pages/requests/RequestRecieved.vue";
+// import UserAuth from "./pages/auth/UserAuth.vue";
+
+const CoachDetails = () => import("./pages/coaches/CoachDetails.vue");
+const CoachRegister = () => import("./pages/coaches/CoachRegister.vue");
+const ContactCoach = () => import("./pages/requests/ContactCoach.vue");
+const RequestRecieved = () => import("./pages/requests/RequestRecieved.vue");
+const UserAuth = () => import("./pages/auth/UserAuth.vue");
 
 import store from "./store/index";
 import NotFound from "./pages/NotFound.vue";

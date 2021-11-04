0068b7e6067e81add633d71eb805e9eb89726093
CyanPiano <cyanp0515@gmail.com>
2021-11-04 03:49:14 -0400

    Assignment 14

--- Vue.js/Advanced/13. Composition API/src/App.vue
+++ Vue.js/Advanced/13. Composition API/src/App.vue
@@ -2,26 +2,78 @@
 	<section class="container">
 		<h2>{{ userName }}</h2>
 		<h2>{{ userName2 }}</h2>
+		<h2>{{ nameAndAge }}</h2>
+
+		<br />
+
+		<h2>{{ user.name }}</h2>
+		<h2>{{ user.age }}</h2>
+		<h2>{{ uname }}</h2>
+
+		<input type="text" v-model="userName">
+
+		<button @click="funcs.changeAge">Change Age</button>
+		<button @click="changeName">Change Name</button>
 	</section>
 </template>
 
-
 <script>
-import { ref } from "vue";
+import { reactive, ref, isRef, isReactive, toRefs, computed, watch } from "vue";
 
 export default {
 	setup() {
-		let userName = ref("the username");
+		let userName = ref("the username"); // seperate refs
 		let userName2 = "the other username";
+		let user = reactive({
+			name: "name",
+			age: 0,
+		});
 
 		setTimeout(() => {
-			userName.value += " - ";
+			userName.value += " - "; // ref object
 			userName2 += " - ";
+			user.name += " - ";
+			user.age++;
+
+			console.log(isRef(userName));
+			console.log(isReactive(user), user.name);
 		}, 5000);
 
-		return { userName, userName2 };
+		// methods
+		function changeAge() {
+			user.age += 10;
+		}
+
+		function changeName() {
+			user.name += " - ";
+		}
+
+		// computed
+		const nameAndAge = computed(() => {
+			return userName.value + " " + user.age;
+		});
+
+		// watchers
+		watch([userName, toRefs(user).age], (o, n) => {
+			console.log(o, n);
+		});
+
+		return {
+			// data
+			userName,
+			userName2,
+			user,
+			uname: toRefs(user).name,
+			
+			// methods
+			changeName,
+			funcs: { changeAge },
+
+			// computed
+			nameAndAge
+		};
 	},
-}
+};
 </script>
 
 <style>

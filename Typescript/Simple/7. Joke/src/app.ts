6164aa4b01a6f7e7f45c102bca797e07146858e7
CyanPiano <cyanp0515@gmail.com>
2021-11-29 04:04:04 -0500

    Fix joke app

--- Typescript/Simple/7. Joke/src/app.ts
+++ Typescript/Simple/7. Joke/src/app.ts
@@ -1,17 +1,24 @@
-// I could have skipped this module
-// This has nothing to do with ts...
-
 import axios from "axios";
 
 const result = document.getElementById("result")! as HTMLParagraphElement;
 const getJoke = document.getElementById("getJoke")! as HTMLButtonElement;
 
+interface response {
+    id: string;
+    joke: string;
+    status: number;
+}
+
 getJoke.onclick = async () => {
-    const res = await axios.get("https://icanhazdadjoke.com/", {
+    const res = await axios.get<response>("https://icanhazdadjoke.com/", {
         headers: {
             "Accept": "application/json"
         }
     });
 
-    result.innerHTML = res.data.joke;
+    if (res.data.status == 200) {
+        result.innerHTML = `<b title="${res.data.id}">${res.data.joke}</b>`;
+    } else {
+        result.innerHTML = "Error!";
+    }
 }

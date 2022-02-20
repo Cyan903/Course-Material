b53933e749e7d2d43e083ee7dcdb0db07296e5f4
CyanPiano <cyanp0515@gmail.com>
2022-02-19 20:51:07 -0500

    Go interface

---
+++ Golang/Simple/5. Interface/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type engbot struct{}
+
+func (engbot) getGreeting() string {
+	return "Hello"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func main() {
+	e := engbot{}
+
+	fmt.Println(e.getGreeting())
+	printGreeting(e)
+}
